120c7ba6f170bea3c91c6bb58d39b4e886f56830
Sasa Stamenkovic <umpirsky@gmail.com>
2012-10-24 22:06:03 +0200

    Updated deps, added some tests.

---
+++ tests/Umpirsky/Silex/Provider/DoctrineExtensionsServiceProviderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of DoctrineExtensionsServiceProvider.
+ *
+ *  (c) Саша Стаменковић <umpirsky@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Umpirsky\Silex\Provider;
+
+use Silex\Application;
+use Palma\Silex\Provider\DoctrineORMServiceProvider;
+use Umpirsky\Silex\Provider\DoctrineExtensionsServiceProvider;
+
+/**
+ * @author Саша Стаменковић <umpirsky@gmail.com>
+ */
+class DoctrineExtensionsServiceProviderTest extends \PHPUnit_Framework_TestCase
+{
+    protected $app;
+
+    protected function setUp()
+    {
+        $this->app = new Application();
+        $this->app->register(new DoctrineORMServiceProvider(), $this['doctrine_orm.options']);
+    }
+
+    public function testRegister()
+    {
+        $this->app->register(new DoctrineExtensionsServiceProvider());
+    }
+}
